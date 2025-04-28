2a4ad7bf3b1f18cf819183a008686677dadddc59
Palak Meena <palakmeena14@gmail.com>
2025-04-28 22:52:44 +0530

    correct image path

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -31,21 +31,21 @@ const programs = [
     title: "MLH Fellowship",
     description: "Global remote internship program for aspiring software engineers, data scientists, and designers.",
     link: "https://fellowship.mlh.io/",
-    image: "/src/images/mlhfellowship.jpg"
+    image: "/images/mlhfellowship.jpg"
   },
 
   {
     title: "Harvard CS50",
     description: "Free online introductory course on Computer Science from Harvard University.",
     link: "https://cs50.harvard.edu/x/",
-    image: "/src/images/harvardcs50.jpg"
+    image: "/images/harvardcs50.jpg"
   },
 
   {
     title: "Hack Club",
     description: "A worldwide community of high school hackers creating amazing tech projects and learning together.",
     link: "https://hackclub.com/",
-    image: "/src/images/harvardcs50.jpg"
+    image: "/images/harvardcs50.jpg"
   },
 
   {
@@ -59,7 +59,7 @@ const programs = [
     title: "Codecademy Learn Paths",
     description: "Guided coding paths to learn web development, data science, and more interactively.",
     link: "https://www.codecademy.com/catalog",
-    image: "/src/images/codecademy.avif"
+    image: "/images/codecademy.avif"
     
   },
 
@@ -74,7 +74,7 @@ const programs = [
     title: "Outreachy Internships",
     description: "Paid remote internships supporting open source for underrepresented communities.",
     link: "https://www.outreachy.org/",
-    image: "/src/images/outreachy.png"
+    image: "/images/outreachy.png"
   },
 
   {
@@ -88,145 +88,145 @@ const programs = [
     title: "Google Summer of Code",
     description: "An annual program sponsored by Google that offers stipends to university students for working on open-source projects.",
     link: "https://summerofcode.withgoogle.com/",
-    image: "/src/images/gssoc.png"
+    image: "/images/gssoc.png"
   },
   {
     title: "IBM SkillsBuild",
     description: "A free platform to help students develop career skills with resources for cloud computing, data science, cybersecurity, and more.",
     link: "https://skillsbuild.org/",
-    image: "/src/images/ibmskillbuild.png"
+    image: "/images/ibmskillbuild.png"
   },
   {
     title: "Udemy Free Courses for Students",
     description: "Udemy offers a collection of free courses for students on topics such as programming, data science, and business.",
     link: "https://www.udemy.com/",
-    image: "/src/images/udemy.png"
+    image: "/images/udemy.png"
   },
   {
     title: "Kaggle Learn",
     description: "Kaggle offers free micro-courses to learn data science and machine learning through practical projects.",
     link: "https://www.kaggle.com/learn",
-    image: "/src/images/kaggle.png"
+    image: "/images/kaggle.png"
   },
   {
     title: "Mozilla Open Leaders Program",
     description: "A program by Mozilla to help students and professionals contribute to open source software and leadership skills.",
     link: "https://foundation.mozilla.org/en/initiatives/open-leaders/",
-    image: "/src/images/mozilla.jpeg"
+    image: "/images/mozilla.jpeg"
   },
   {
     title: "Turing Fellowship",
     description: "A fellowship program providing remote internships for talented students interested in artificial intelligence and software development.",
     link: "https://www.turing.com/fellowship",
-    image: "/src/images/turingfellows.jpeg"
+    image: "/images/turingfellows.jpeg"
   },
   {
     title: "Coursera for Students",
     description: "Coursera offers free and discounted access to its courses for students, covering various tech, business, and personal development topics.",
     link: "https://www.coursera.org/",
-    image: "/src/images/coursera.png"
+    image: "/images/coursera.png"
   },
   {
     title: "Google AI Residency Program",
     description: "A 12-month research training role designed to jumpstart careers in AI research.",
     link: "https://ai.google/education/ai-residency/",
-    image: "/src/images/googleresidency.jpg"
+    image: "/images/googleresidency.jpg"
   },
   {
     title: "AWS Activate for Startups",
     description: "AWS Activate provides startups with the resources they need to scale and grow with AWS credits, training, and support.",
     link: "https://aws.amazon.com/activate/",
-    image: "/src/images/awsactivate.png"
+    image: "/images/awsactivate.png"
   },
   {
     title: "Facebook Developer Circles",
     description: "Join a community of developers to collaborate on projects, learn new skills, and connect with other developers globally.",
     link: "https://developers.facebook.com/developercircles/",
-    image: "/src/images/facebookcircle.jpg"
+    image: "/images/facebookcircle.jpg"
   },
   {
     title: "Codewars",
     description: "A community-driven platform for learning programming through challenges and coding kata.",
     link: "https://www.codewars.com/",
-    image: "/src/images/codewars.jpeg"
+    image: "/images/codewars.jpeg"
   },
   {
     title: "Women Who Code",
     description: "A global community that inspires women to excel in technology careers with events, resources, and networking opportunities.",
     link: "https://www.womenwhocode.com/",
-    image: "/src/images/womenwhocode.png"
+    image: "/images/womenwhocode.png"
   },
   {
     title: "Techstars Startup Programs",
     description: "Mentorship-driven accelerator programs to help startups succeed by connecting them with investors, mentors, and partners.",
     link: "https://www.techstars.com/",
-    image: "/src/images/techstars.png"
+    image: "/images/techstars.png"
   },
   {
     title: "OpenAI Fellowship",
     description: "A fellowship that aims to support underrepresented groups in AI by providing them with opportunities to advance in the field.",
     link: "https://openai.com/research/fellowship",
-    image: "/src/images/openai.jpeg"
+    image: "/images/openai.jpeg"
   },
   {
     title: "Stack Overflow for Teams",
     description: "A collaboration tool for teams to share knowledge, manage questions, and answer technical queries within their organization.",
     link: "https://stackoverflow.com/teams",
-    image: "/src/images/stakeoverflow.png"
+    image: "/images/stakeoverflow.png"
   },
   {
     title: "Mozilla Developer Network (MDN) Web Docs",
     description: "Free and open documentation for web developers, covering HTML, CSS, JavaScript, and more.",
     link: "https://developer.mozilla.org/",
-    image: "/src/images/mdn.png"
+    image: "/images/mdn.png"
   },
   {
     title: "Leetcode",
     description: "A platform for preparing for coding interviews through algorithm challenges and problems.",
     link: "https://leetcode.com/",
-    image: "/src/images/leetcode.jpeg"
+    image: "/images/leetcode.jpeg"
   },
   {
     title: "AI for Good",
     description: "A global movement that brings together individuals, organizations, and tech innovators to apply AI to solve societal challenges.",
     link: "https://www.ai4good.org/",
-    image: "/src/images/aiforgood.jpeg"
+    image: "/images/aiforgood.jpeg"
   },
   {
     title: "GitLab for Education",
     description: "Free access to GitLab’s premium features for students and educators to foster collaboration and version control for software projects.",
     link: "https://about.gitlab.com/education/",
-    image: "/src/images/githubeducation.png"
+    image: "/images/githubeducation.png"
   },
   {
     title: "Devpost",
     description: "A platform for participating in hackathons, coding competitions, and discovering new tech projects.",
     link: "https://devpost.com/",
-    image: "/src/images/devpost.png"
+    image: "/images/devpost.png"
   },
   {
     title: "Code for America Fellowship",
     description: "A paid fellowship program for technologists to work with local governments to improve public services using tech.",
     link: "https://www.codeforamerica.org/fellowship",
-    image: "/src/images/codeforamerica.png"
+    image: "/images/codeforamerica.png"
   },
   {
     title: "MIT OpenCourseWare",
     description: "Free online courses from MIT, offering video lectures, assignments, and more on various topics in computer science and other fields.",
     link: "https://ocw.mit.edu/",
-    image: "/src/images/mit.png"
+    image: "/images/mit.png"
   },
   {
     title: "Startup Grind Global Conference",
     description: "A global conference for entrepreneurs to network, learn, and share knowledge on growing startups and building businesses.",
     link: "https://www.startupgrind.com/",
-    image: "/src/images/startupgrindconference.png"
+    image: "/images/startupgrindconference.png"
   },
   {
     title: "UNESCO Open Education",
     description: "A platform providing free resources for global learning in education, focusing on making quality education accessible to all.",
     link: "https://en.unesco.org/themes/open-education",
-    image: "/src/images/unesco.png"
+    image: "/images/unesco.png"
   }
   // Add more program cards here
 ];
